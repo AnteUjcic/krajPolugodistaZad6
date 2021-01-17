0f621c9683396b697b7bf5033d4ad63638b8f2fe
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-01-17 15:33:59 +0100

    Add files via upload

---
+++ zadatak6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n,x=0;
+            
+
+
+            Console.WriteLine("upisite broj do kuda ce se zbrajati: ");
+            n=Convert.ToInt64(Console.ReadLine());
+            for(long i = 0; i <= n; i++)
+            {
+                
+                x=x+i;
+                
+            }
+            Console.Write("Zbroj prvih ");
+            Console.Write(n);
+            Console.Write(" brojeva je ");
+            Console.Write(x);
+            Console.ReadKey();
+
+        }
+    }
+}
